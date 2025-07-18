6407726c3662e95e3a542d1fdbff8ed142c889cb
Tommy K <140900186+tommy-ca@users.noreply.github.com>
2025-07-18 16:11:58 +0200

    Fixing the utils init file to only import modules that exist

--- src/crypto_lakehouse/utils/__init__.py
+++ src/crypto_lakehouse/utils/__init__.py
@@ -1,8 +1,6 @@
 """Utility functions for the crypto data lakehouse."""
 
 from .gap_detection import GapDetector
-from .data_merger import DataMerger
 from .resampler import DataResampler
-from .query_engine import DuckDBQueryEngine
 
-__all__ = ["GapDetector", "DataMerger", "DataResampler", "DuckDBQueryEngine"]
+__all__ = ["GapDetector", "DataResampler"]
