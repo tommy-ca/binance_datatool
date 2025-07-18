9c36d5248ca52775bcd3e8ff4f81e89b894ceb95
Tommy K <140900186+tommy-ca@users.noreply.github.com>
2025-07-18 16:14:58 +0200

    Updating utils init file to include query engine exports

--- src/crypto_lakehouse/utils/__init__.py
+++ src/crypto_lakehouse/utils/__init__.py
@@ -2,5 +2,12 @@
 
 from .gap_detection import GapDetector
 from .resampler import DataResampler
+from .query_engine import DuckDBQueryEngine, QueryConfig, create_query_engine
 
-__all__ = ["GapDetector", "DataResampler"]
+__all__ = [
+    "GapDetector", 
+    "DataResampler", 
+    "DuckDBQueryEngine", 
+    "QueryConfig", 
+    "create_query_engine"
+]
